cde4fc0d2c3ae752444a8a736095825756ae1f52
jdoodle <jdoodle450@gmail.com>
2025-09-09 20:23:51 +0000

    Set previous webapp icon

--- src/components/pro-assignment-icon.tsx
+++ src/components/pro-assignment-icon.tsx
@@ -3,9 +3,7 @@ import { cn } from '@/lib/utils';
 import Image from 'next/image';
 
 export function ProAssignmentIcon({ className }: { className?: string }) {
-  // A valid default icon is used here to prevent crashes.
-  // To use your own icon, upload it to a service like Postimages.org,
-  // get the "Direct Link", and paste it here.
+  // This is the icon URL for the web application.
   const iconUrl = "https://i.postimg.cc/VNmhm0H5/IMG-20250819-WA0033.jpg";
 
   return (

e8286111ce7cf281b4a6f7fdee446a2db93849ab
jdoodle <jdoodle450@gmail.com>
2025-09-10 08:22:52 +0000

    How to add my own link now there is local host link

--- next.config.ts
+++ next.config.ts
@@ -32,6 +32,8 @@ const nextConfig: NextConfig = {
    env: {
     NEXT_PUBLIC_ADMIN_EMAIL: 'itxprince440@gmail.com',
     GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+    // When you deploy your app, set NEXT_PUBLIC_BASE_URL in your hosting environment
+    // to your app's public URL.
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:9002',
   },
 };
